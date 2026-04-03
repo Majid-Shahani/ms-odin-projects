594e00164380b5082953962465cb51246b5aed8f
Majid-Shahani <majid.shahani.1390@gmail.com>
2026-04-03 11:44:23 +0300

    Add resize, clear, and rainbow to pad

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,33 +1,92 @@
 /*
- * Constants
+ * Globals
  */
-const GRID_WIDTH = 16;
-
+let gridWidth = 16;
+let color = "cyan";
+let mouseDown = false;
+let currentKey = 0; // M1
 /*
  * Functions
  */
-function createElements() {
-  const grid = document.querySelector(".sketchpad");
+function setupEvents() {
+  document.addEventListener("mousedown", (e) => {
+    mouseDown = true;
+    currentKey = e.button;
+  });
 
-  for (let i = 0; i < GRID_WIDTH; i++) {
-    let gridRow = document.createElement("div");
+  document.addEventListener("mouseup", (e) => {
+    mouseDown = false;
+    currentKey = 0;
+  });
+}
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.round(Math.random() * 16)];
+  }
+  return color;
+}
+
+function createGrid(width) {
+  if (width >= 100) width = 100;
+  const pad = document.querySelector(".sketchpad");
+  const grid = [];
+
+  for (let i = 0; i < width; i++) {
+    const gridRow = document.createElement("div");
     gridRow.setAttribute("class", "gridRow");
 
-    for (let j = 0; j < GRID_WIDTH; j++) {
-      let div = document.createElement("div");
-      div.setAttribute("class", "fragment");
-      gridRow.appendChild(div);
+    if (i === 0) gridRow.setAttribute("style", "border-top-width: 1px");
+    else if (i === width - 1)
+      gridRow.setAttribute("style", "border-bottom-width: 1px");
+
+    for (let j = 0; j < width; j++) {
+      const frag = document.createElement("div");
+      frag.setAttribute("class", "fragment");
+      frag.addEventListener("mouseenter", (e) => {
+        if (mouseDown) {
+          if (currentKey === 0) e.target.style.background = color;
+          else e.target.style.background = getRandomColor();
+        }
+      });
+      gridRow.appendChild(frag);
     }
 
-    grid.appendChild(gridRow);
+    grid.push(gridRow);
   }
+  pad.replaceChildren(...grid);
+  gridWidth = width;
+}
+
+function createControlPanel() {
+  const panel = document.querySelector(".controls");
+
+  // Add Resize Button
+  const sizeButton = document.createElement("button");
+  sizeButton.textContent = "Resize Grid";
+  sizeButton.setAttribute("class", "button");
+  sizeButton.addEventListener("click", () => {
+    createGrid(+prompt("Set Grid Size", "16"));
+  });
+  panel.appendChild(sizeButton);
+
+  // Add Clear Button
+  const clear = document.createElement("button");
+  clear.textContent = "Clear";
+  clear.setAttribute("cleass", "button");
+  clear.addEventListener("click", () => createGrid(gridWidth));
+  panel.appendChild(clear);
 }
 
 /*
  * Main
  */
 function main() {
-  createElements();
-  //addEventListeners()
+  setupEvents();
+  createGrid(gridWidth);
+  createControlPanel();
 }
 main();
