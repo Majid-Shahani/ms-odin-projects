eef21063d4b390101fc6a6a8d52e1095202295f1
Majid-Shahani <majid.shahani.1390@gmail.com>
2026-04-22 16:56:20 +0300

    Add eslint, prettier, webpack-merge

---
+++ template/eslint.config.js
@@ -0,0 +1,52 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+//import react from "eslint-plugin-react";
+//import reactHooks from "eslint-plugin-react-hooks";
+
+export default defineConfig([
+  {
+    ignores: ["node_modules/**", "dist/**"],
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        //...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+  js.configs.recommended,
+  eslintConfigPrettier,
+  //{
+  //  files: ["**/*.{js,jsx}"],
+  //  plugins: {
+  //    react,
+  //    "react-hooks": reactHooks,
+  //  },
+  //  languageOptions: {
+  //    parserOptions: {
+  //      ecmaFeatures: {
+  //        jsx: true,
+  //      },
+  //    },
+  //  },
+  //  rules: {
+  //    ...react.configs.recommended.rules,
+  //    ...reactHooks.configs.recommended.rules,
+  //  },
+  //  settings: {
+  //    react: {
+  //      version: "detect",
+  //    },
+  //  },
+  //},
+]);
